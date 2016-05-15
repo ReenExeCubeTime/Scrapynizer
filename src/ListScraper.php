133642a764b97a1b932ff9140006f7177f0c7ca8
ReenExe <ipourri@gmail.com>
2016-05-15 20:51:06 +0300

    refactoring

---
+++ src/ListScraper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ReenExe\Scrapynizer;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Symfony\Component\DomCrawler\Crawler;
+
+class ListScraper
+{
+    const STATUS_PROGRESS = 0;
+
+    const STATUS_END = 1;
+
+    /**
+     * @var ListContentRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var PaginationHunterInterface
+     */
+    private $pager;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function process($limit)
+    {
+        if ($last = $this->repository->getLast()) {
+            $nextPath = $this->pager->getNextPage(new Crawler($last));
+
+            if (empty($nextPath)) {
+                return self::STATUS_END;
+            }
+
+        } else {
+            $nextPath = $this->pager->getFirstPage();
+        }
+
+        do {
+            try {
+                $html =  $this->client->get($nextPath)->getBody()->getContents();
+            } catch (ClientException $e) {
+                return self::STATUS_END;
+            }
+
+            $crawler = new Crawler($html);
+
+            $this->repository->save($nextPath, $html);
+
+            $nextPath = $this->pager->getNextPage($crawler);
+        } while (--$limit && $nextPath);
+
+        return self::STATUS_PROGRESS;
+    }
+}
